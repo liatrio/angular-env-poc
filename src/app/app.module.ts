9c4a6eb9e6d2356ed781677dbe5f24ac77578741
Benjamin Stein <ben@liatrio.com>
2018-11-14 16:48:41 -0500

    add envservice to the providers list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { EnvServiceProvider } from './env.service.provider'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EnvServiceProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
