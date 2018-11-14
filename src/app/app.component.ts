803f75cd12c849e303a085c70091e9340bd346c8
Benjamin Stein <ben@liatrio.com>
2018-11-14 16:45:11 -0500

    add envservice to the providers list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EnvService } from './env.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'boom';
+    constructor(
+        private env: EnvService
+    ) {
+     console.log('using '+ env.apiUrl);
+ }
+  title = this.env.apiUrl;
 }
