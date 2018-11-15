f75334c0ac48a559b16a047d5d492570ad8c3478
Benjamin Stein <ben@liatrio.com>
2018-11-15 09:33:14 -0500

    add environment specific configs

--- envConfigs/prod.js
+++ envConfigs/prod.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'http://localhost.com';
+  window.__env.apiUrl = 'http://prod.com';
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
